f3727189bf64b19eca4bfe9ca8637b9dbfa24ca2
Maisam Afshar <maisam.afshar@gmail.com>
2024-07-11 15:27:13 +0500

    ref: icon component

--- components/icon.tsx
+++ components/icon.tsx
@@ -1,18 +1,23 @@
-import { icons, LucideProps } from 'lucide-react-native';
-import React from 'react';
-import { ColorValue } from 'react-native';
+import { icons, LucideProps } from "lucide-react-native";
+import React, { SVGProps } from "react";
+import { ColorValue } from "react-native";
 
 interface IconProps {
   name: keyof typeof icons;
   color?: ColorValue;
-  size?: LucideProps['size'];
-  isGray?: boolean;
+  size?: LucideProps["size"];
+  [key: string]: any;
 }
+const CustomIcon: React.FC<IconProps> = ({
+  name,
+  color = "#A4A8B5",
+  size,
+  ...props
+}) => {
+  const LucideIcon: React.FC<LucideProps & { color?: ColorValue }> =
+    icons[name];
 
-const CustomIcon: React.FC<IconProps> = ({ name, color, size, isGray }) => {
-  const LucideIcon: React.FC<LucideProps & { color?: ColorValue }> = icons[name];
-
-  return <LucideIcon color={isGray ? '#A4A8B5' : color} size={size} />;
+  return <LucideIcon color={color} size={size} {...props} />;
 };
 
 export default CustomIcon;
