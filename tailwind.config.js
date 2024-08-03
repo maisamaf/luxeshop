9c2273b995c9d983a958940e84bb84fcef7171fc
Maisam Afshar <maisam.afshar@gmail.com>
2024-08-03 16:12:12 +0500

    init config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
+      fontFamily: {
+        sfregular: ["sfregular", "sans-serif"],
+        sfsemibold: ["sfsemibold", "sans-serif"],
+        sfbold: ["sfbold", "sans-serif"],
+        "sfregular-italic": ["sfregular-italic", "sans-serif"],
+      },
       colors: {
         primary: '#FA5A2A',
         black: '#030711',
