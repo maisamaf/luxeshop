8c8accaf1ddab786c15c2563b24a19977459d696
Maisam Afshar <maisam.afshar@gmail.com>
2024-08-05 15:53:25 +0500

    update main layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,38 +1,41 @@
-import { Link, Tabs } from 'expo-router';
+import { Tabs } from "expo-router";
 
-import { HeaderButton } from '../../components/HeaderButton';
-import { TabBarIcon } from '../../components/TabBarIcon';
-import { Pressable, Text, TouchableOpacity, View } from 'react-native';
-import { ShoppingBag } from 'lucide-react-native';
-import CustomIcon from "@/components/icon"
-import { StatusBar } from 'expo-status-bar';
+import { TabBarIcon } from "../../components/TabBarIcon";
+import { Text, TouchableOpacity, View } from "react-native";
+
+import CustomIcon from "@/components/icon";
+
+import { PRIMARY } from "@/utils/colors";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: '#FA5A2A',
-      }}>
+        tabBarActiveTintColor: PRIMARY,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           headerShown: false,
           tabBarIcon: ({ color }) => <TabBarIcon name="House" color={color} />,
           headerLeft: () => {
-            return <View className='flex-row items-center gap-2 ml-4'>
-              <CustomIcon name='Chrome' color="#FA5A2A" size={32} />
-              <Text className='font-semibold'>Luxeshop</Text>
-            </View>
+            return (
+              <View className="flex-row items-center gap-2 ml-4">
+                <CustomIcon name="Chrome" color={PRIMARY} size={32} />
+                <Text className="font-semibold">Luxeshop</Text>
+              </View>
+            );
           },
           headerRight: () => (
-            <View className='flex-row gap-x-2.5 mx-4'>
-            <TouchableOpacity className='items-center justify-center p-1.5 border rounded-md border-grey/40'>
-            <CustomIcon name="ShoppingBag" size={16} isGray />
-            </TouchableOpacity>
-            <TouchableOpacity className='items-center justify-center p-1.5 border rounded-md border-grey/40'>
-            <CustomIcon name="Bell" size={16} isGray />
-            </TouchableOpacity>
+            <View className="flex-row gap-x-2.5 mx-4">
+              <TouchableOpacity className="items-center justify-center p-1.5 border rounded-md border-grey/40">
+                <CustomIcon name="ShoppingBag" size={16} isGray />
+              </TouchableOpacity>
+              <TouchableOpacity className="items-center justify-center p-1.5 border rounded-md border-grey/40">
+                <CustomIcon name="Bell" size={16} isGray />
+              </TouchableOpacity>
             </View>
           ),
         }}
@@ -40,25 +43,26 @@ export default function TabLayout() {
       <Tabs.Screen
         name="wishlist"
         options={{
-          title: 'Wishlist',
+          title: "Wishlist",
           tabBarIcon: ({ color }) => <TabBarIcon name="Heart" color={color} />,
         }}
       />
       <Tabs.Screen
         name="transaction"
         options={{
-          title: 'Transaction',
-          tabBarIcon: ({ color }) => <TabBarIcon name="FileText" color={color} />,
+          title: "Transaction",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="FileText" color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
+          title: "Profile",
           tabBarIcon: ({ color }) => <TabBarIcon name="User" color={color} />,
         }}
       />
-
     </Tabs>
   );
 }
